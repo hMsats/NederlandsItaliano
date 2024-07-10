2f50ecdfb1c471d22a8d4ce416d411d3496d4042
hMsats <millibitcoins@gmail.com>
2024-07-10 18:16:22 +0200

    Geen versienummer

--- maak_nedit.sh
+++ maak_nedit.sh
@@ -8,7 +8,7 @@ echo "Hiertussen mag niets komen:"
 
 echo "Controleer ook met % in vi: [ en ] plus ( en ) zie begin en einde data.html"
 
-cat nederlandsitaliano_6.0.0_boven.html data.html nederlandsitaliano_6.0.0_onder.html > index.html
+cat nederlandsitaliano_boven.html data.html nederlandsitaliano_onder.html > index.html
 
 aw=$(cat data.html | grep "\[\"" | wc -l)
 echo "Aantal items: $aw"
