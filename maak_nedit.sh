74dc8cb26f4ba064e510138a5b74a39a37348a2a
hMsats <millibitcoins@gmail.com>
2024-07-12 13:05:18 +0200

    Extra controles

--- maak_nedit.sh
+++ maak_nedit.sh
@@ -10,3 +10,13 @@ cat nederlandsitaliano_boven.html data.html nederlandsitaliano_onder.html > inde
 
 aw=$(cat data.html | grep "\[\"" | wc -l)
 echo "Aantal items: $aw"
+
+# Het aantal "," gedeeld door 2
+aux=$(cat data.html | grep "\",\"" | wc -l)
+res1=$(echo "scale=0;$aux/2" | bc -l)
+echo "Controle    : $res1"
+
+res2=$(cat data.html | grep '^\["\\' | wc -l)
+aux=$(cat data.html | grep '"\],\["\\' | wc -l)
+res3=$(echo "scale=0;($aux+$res2)/1" | bc -l)
+echo "Controle    : $res3"
