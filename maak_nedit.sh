ea94539f6d886b3093df8c4dca8e0ebfcfb472e3
hMsats <millibitcoins@gmail.com>
2024-07-12 14:45:07 +0200

    Netjes

--- maak_nedit.sh
+++ maak_nedit.sh
@@ -14,10 +14,10 @@ echo "Aantal voorbeelden: $aw"
 # Het aantal "," gedeeld door 2
 aux=$(cat data.html | grep "\",\"" | wc -l)
 res1=$(echo "scale=0;$aux/2" | bc -l)
-echo "Controle    : $res1"
+echo "Controle          : $res1"
 
 # Het aantal voorbeelden moet gelijk zijn aan aantal "],[" plus het aantal (aan het begin van een regel) [\ 
 res2=$(cat data.html | grep '^\["\\' | wc -l)
 aux=$(cat data.html | grep '"\],\["\\' | wc -l)
 res3=$(echo "scale=0;($aux+$res2)/1" | bc -l)
-echo "Controle    : $res3"
+echo "Controle          : $res3"
