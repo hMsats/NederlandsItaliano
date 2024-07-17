c9a89ca0351ede01b4031f39238c01d25cddf83a
hMsats <millibitcoins@gmail.com>
2024-07-17 16:58:17 +0200

    Einde van een regel beter controleren

--- controleer.sh
+++ controleer.sh
@@ -1,4 +1,5 @@
-cat data.html | grep -v br | grep -v "," | grep -v aantal_voorbeelden | grep -v begin | grep -v einde | grep -v "data=" | grep -v "zinnen en los" | grep -v "\[" | grep -v "\]" | grep -v "DATA EINDIGT HIER" | grep -v "DATA BEGINT HIER"
+# Controleer dat elke regel (op uitzonderingen na) aan het eind een <br>\ heeft
+cat data.html | grep -v "<br/>\\\\$" | grep -v "einde: " | grep -v "begin: " | grep -v '"\],\["\\' | grep -v '","\\' | grep -v 'aantal_voorbeelden' | grep -v 'data=data.concat' | grep -v "\[" | grep -v "\]" | grep -v "DATA EINDIGT HIER" | grep -v "DATA BEGINT HIER"
 
 # controleer dat elke regel (op uitzonderingen na) een backslash heeft
 cat data.html | grep -v "\\\\" | grep -v "data." | grep -v "]]);" | grep -v "]];" | grep -v "//" | grep -v "]);" | grep -v "\"]"
