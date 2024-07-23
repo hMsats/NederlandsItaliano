7951ed746364a4a3d68bb9362feb2b9a88ac6da1
hMsats <millibitcoins@gmail.com>
2024-07-23 17:42:11 +0200

    Betere controles en betere meldingen

--- maak_nedit.sh
+++ maak_nedit.sh
@@ -19,7 +19,23 @@ res1=$(echo "scale=0;$aux/2" | bc -l)
 echo "Controle          : $res1"
 
 # Het aantal voorbeelden moet gelijk zijn aan aantal "],[" plus het aantal (aan het begin van een regel) [\ 
-res2=$(cat data.html | grep '^\["\\' | wc -l)
-aux=$(cat data.html | grep '"\],\["\\' | wc -l)
-res3=$(echo "scale=0;($aux+$res2)/1" | bc -l)
-echo "Controle          : $res3"
+aux1=$(cat data.html | grep '^\["\\' | wc -l)
+aux2=$(cat data.html | grep '"\],\["\\' | wc -l)
+res2=$(echo "scale=0;($aux2+$aux1)/1" | bc -l)
+echo "Controle          : $res2"
+
+if [ $aw -ne $res1 ]; then
+  echo -n "ER IS EEN FOUT OPGETREDEN:"
+  echo "  Aantal voorbeelden niet gelijk aan eerste controle"
+  echo "Tip: diff index.html index_bew_1.html"
+fi
+if [ $aw -ne $res2 ]; then
+  echo -n "ER IS EEN FOUT OPGETREDEN:"
+  echo "  Aantal voorbeelden niet gelijk aan tweede controle"
+  echo "Tip: diff index.html index_bew_1.html"
+fi
+if [ $res1 -ne $res2 ]; then
+  echo -n "ER IS EEN FOUT OPGETREDEN:"
+  echo "  De controles zijn niet gelijk"
+  echo "Tip: diff index.html index_bew_1.html"
+fi
