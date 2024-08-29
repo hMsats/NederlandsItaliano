92a6a74c19a837fcd99a51ec14af3cf4307097a8
hMsats <millibitcoins@gmail.com>
2024-08-29 22:09:38 +0200

    <br/> -> <br>

--- controleer.sh
+++ controleer.sh
@@ -1,14 +1,14 @@
 # Controleer dat elke regel (op uitzonderingen na) aan het eind een <br>\ heeft
-cat data.html | grep -v "<br/>\\\\$" | grep -v "einde: " | grep -v "begin: " | grep -v '"\],\["\\' | grep -v '","\\' | grep -v 'aantal_voorbeelden' | grep -v 'data=data.concat' | grep -v "\[" | grep -v "\]" | grep -v "DATA EINDIGT HIER" | grep -v "DATA BEGINT HIER"
+cat data.html | grep -v "<br>\\\\$" | grep -v "einde: " | grep -v "begin: " | grep -v '"\],\["\\' | grep -v '","\\' | grep -v 'aantal_voorbeelden' | grep -v 'data=data.concat' | grep -v "\[" | grep -v "\]" | grep -v "DATA EINDIGT HIER" | grep -v "DATA BEGINT HIER"
 
 # controleer dat elke regel (op uitzonderingen na) een backslash heeft
 cat data.html | grep -v "\\\\" | grep -v "data." | grep -v "]]);" | grep -v "]];" | grep -v "//" | grep -v "]);" | grep -v "\"]"
 
 # controleer dat nieuwe_pagina goed geschreven is
-cat data.html | grep "_" | grep -v '^nieuwe_pagina<br\/>\\' | grep -v aantal_voorbeelden
+cat data.html | grep "_" | grep -v '^nieuwe_pagina<br>\\' | grep -v aantal_voorbeelden
 
 # controleer dat ieder item een nummer heeft
-cat data.html | grep -B 2 '"\],\["\\' | grep '<br/>\\' | sed 's/<br\/>\\//' | sed '/[0-9]/d'
+cat data.html | grep -B 2 '"\],\["\\' | grep '<br>\\' | sed 's/<br>\\//' | sed '/[0-9]/d'
 
 # controleer dat er geen spatie achter het einde van een regel staat
-cat data.html | grep '<br/>\\ '
+cat data.html | grep '<br>\\ '
